b77806abb71abcc698d890fa30ae221f7648ac7d
sayyadalizadeh <alizadeh_sa1981@yahoo.com>
2025-10-09 02:20:37 +0300

    Clean root folder, keep Arrays and Lists only

--- ArrayExample.java
+++
@@ -1,9 +0,0 @@
-public class ArrayExample {
-    public static void main(String[] args) {
-        int[] numbers = {10, 20, 30, 40, 50};
-
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println("Element at index " + i + ": " + numbers[i]);
-        }
-    }
-}
